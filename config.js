7dbb9f6b8c7130ea76cf96ca766d163adf7ed6c8
Mohammad Saif <saifsami321md@gmail.com>
2022-12-14 20:12:43 +0530

    crud operation practice under backtics

--- config.js
+++ config.js
@@ -6,3 +6,116 @@ const connection= mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports=connection;
+
+/*
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const connection = mongoose.connect(process.env.PORT)
+
+module.exports = connection
+
+//.env
+PORT=8080
+MONGO_URL = ''
+
+//index.js
+const express = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const connection = require("./config")
+
+app.get('/',(req,res)=>{
+    res.send("Welcome from crud Operation")
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,async()=>{
+    try{
+        await connection
+        console.log("db connected")
+    }
+    catch(err){
+     console.log('check config',err)      
+    }
+    console.log(`listening on port ${PORT}`)
+})
+
+//models
+
+const mongoose = require('mongoose')
+
+const CrudSchema = new mongoose.Schema({
+    "name":"String",
+    "age":Number,
+    "City":"String"
+})
+
+const CrudModel = mongoose.model('crud1',CrudSchema)
+
+module.exports = CrudModel
+
+//routes
+
+const express = require('mongoose')
+
+const CrudModel = require('./models/Crud.model')
+
+const CrudRouter = express.Router()
+
+CrudRouter.get('/',async(req,res)=>{
+    try{
+         const user = await CrudModel.find()
+         res.send(user)
+    }
+    catch(err){
+        console.log({message:err.message})
+
+    }
+})
+
+CrudRouter.post('/post',async(req,res)=>{
+    const {name,age,city} = req.body;
+    try{
+         const user = await CrudModel.create({name,age,city})
+         user.save();
+         res.status(200).send(user)
+    }
+    catch(err){
+       res.status(500).send({message:err.message})
+    }
+})
+
+
+CrudRouter.delete('delete/:id',async(req,res)=>{
+    const userId = req.params.id
+    try{
+        const user = await CrudModel.findByIdAndDelete(_id:userId)
+        res.status(200).send(user)
+    }
+    catch(err){
+       res.status(500).send({message:err.message})
+    }
+})
+
+CrudRouter.update('edit/:id',async(req,res)=>{
+    try{
+        const user = CrudModel.finfByIdAndUpdate(req.params.id,req.body)
+        user.save()
+        res.status(200).send(user)
+    }
+    catch(err){
+       res.status(500).send({message:err.message})
+    }
+})
+
+module.exports = CrudRouter
+
+
+*/
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express= require('express');
 const cors= require('cors');
-const connection= require('./config.js');
-const crudRouter = require('./routes/Crud.routes.js');
 require('dotenv').config();
 
 const app= express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+const connection= require('./config.js');
+const crudRouter = require('./routes/Crud.routes.js');
+
 
 
 app.get('/',(req,res)=>{
@@ -28,3 +30,34 @@ app.listen(PORT,async()=>{
     }
     console.log(`listening on port ${PORT}`)
 })
+
+
+/*
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const connection= require("./config")
+
+app.get('/',(req,res)=>{
+    res.send("Welcome from Crud Operation")
+})
+
+const PORT= process.env.PORT || 8080
+
+app.listen(PORT,async()=>{
+    try{
+         await connection
+         console.log('db connected')
+    }
+    catch(err){
+         console.log('check config',err)
+    }
+    console.log(`listening on port ${PORT}`)
+})
+md mayan--->slack
+*/
